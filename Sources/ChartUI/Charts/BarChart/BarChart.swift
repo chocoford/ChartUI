0cb0da2ba4c69cd50cb62e20d5964375e48a20d3
chocoford <504001583@qq.com>
2022-01-19 16:14:12 +0800

    update for multi-bar bar chart support

--- Sources/ChartUI/Charts/BarChart/BarChart.swift
+++ Sources/ChartUI/Charts/BarChart/BarChart.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 public struct BarChart: View {
-    @ObservedObject public var chartDataset: ChartDataset
+    @EnvironmentObject public var chartDataset: ChartDataset
     @EnvironmentObject public var options: ChartOptions
     @State private var touchLocation: CGFloat = -1.0
     
@@ -10,46 +10,49 @@ public struct BarChart: View {
         static let spacing: CGFloat = 16.0
     }
     
-    public init(chartDataset: ChartDataset) {
-        self.chartDataset = chartDataset
-    }
-    
+
     /// The content and behavior of the `BarChartRow`.
     ///
     /// Shows each `BarChartCell` in an `HStack`; may be scaled up if it's the one currently being touched.
     /// Not using a drawing group for optimizing animation.
     /// As touched (dragged) the `touchLocation` is updated and the current value is highlighted.
     public var body: some View {
-        ChartContainerView(data: chartDataset) { geometry, maxValue in
+        ChartContainerView { geometry, maxValue in
             let spacing: CGFloat = (geometry.size.width) / CGFloat(chartDataset.labels.count * 3)
             HStack(alignment: .bottom, spacing: spacing) {
-                ForEach(Array(chartDataset.labels.enumerated()), id: \.0) { (index, _) in
+                ForEach(Array(chartDataset.labels.enumerated()), id: \.0) { (dataIndex, _) in
                     /// Value relative to maximum value
                     HStack(alignment: .bottom, spacing: spacing / 5) {
-                        ForEach(chartDataset.data) { dataset in
+                        ForEach(Array(chartDataset.data.enumerated()), id: \.1.id) { (datasetIndex, dataset) in
                             /// leave those `nodata` alone
-                            if index < dataset.data.count {
-                                let normalizedValue: Double = (dataset.data[index] ?? 0.0) / Double(maxValue)
+                            if dataIndex < dataset.data.count {
+                                let dataValue = dataset.data[dataIndex] ?? 0.0
+                                let normalizedValue: Double = dataValue / Double(maxValue)
                                 ZStack(alignment: .bottom) {
                                     BarChartCell(value: normalizedValue,
-                                                 index: index,
+                                                 index: dataIndex,
                                                  backgroundColor: dataset.backgroundColor,
                                                  borderColor: dataset.borderColor,
                                                  borderWdith: dataset.borderWidth,
-                                                 touchLocation: self.touchLocation)
-//                                    if options.dataset.showValue {
-//                                        let offset: CGFloat = CGFloat(normalizedValue) * geometry.size.height
-//                                        let valueHeight: CGFloat = 32
-//                                        HStack() {
-//                                            Text("123")
-//                                        }
-//                                        .padding(.bottom, offset - valueHeight)
-//                                        .border(.red)
-//                                    }
+                                                 touchLocation: self.touchLocation,
+                                                 showDelay: Double(datasetIndex) * 0.2)
+                                    // TODO: 不是很完美的解决方案
+                                    if options.dataset.showValue {
+                                        GeometryReader { geometry in
+                                            let offset: CGFloat = CGFloat(1 - normalizedValue) * geometry.size.height
+                                            VStack(spacing: 0) {
+                                                Spacer().frame(height: offset - 16)
+                                                Text(String(dataValue))
+                                                    .font(.footnote)
+                                                    .fixedSize()
+                                                    .animation(Animation.spring(), value: offset)
+                                                    .transition(.opacity)
+                                            }.frame(width: geometry.size.width)
+                                        }
+                                    }
                                 }
-//                                .border(.white)
-                                //                                       .scaleEffect(getScaleSize(touchLocation: self.touchLocation, index: index), anchor: .bottom)
-                                    .animation(Animation.easeInOut(duration: 0.2), value: chartDataset.labels)
+                                .scaleEffect(getScaleSize(touchLocation: self.touchLocation, index: dataIndex), anchor: .bottom)
+                                .animation(Animation.easeInOut(duration: 0.2), value: chartDataset.labels)
                             } else {
                                 BarChartCell(value: 0, backgroundColor: Color.clear, borderColor: Color.clear, borderWdith: 0, touchLocation: 0)
                             }
@@ -57,21 +60,21 @@ public struct BarChart: View {
                     }
                 }
             }
-                   .padding(.horizontal, spacing / 2)
-                   .gesture(DragGesture()
-                                .onChanged({ value in
-                       let width = geometry.frame(in: .local).width
-                       self.touchLocation = value.location.x/width
-                       //                    if let currentValue = self.getCurrentValue(width: width) {
-                       //                        self.chartValue.currentValue = currentValue
-                       //                        self.chartValue.interactionInProgress = true
-                       //                    }
-                   })
-                                .onEnded({ value in
-                       //                    self.chartValue.interactionInProgress = false
-                       self.touchLocation = -1
-                   })
-                   )
+            .padding(.horizontal, spacing / 2)
+            .gesture(DragGesture()
+                        .onChanged({ value in
+                let width = geometry.size.width
+                self.touchLocation = value.location.x / width
+                //                    if let currentValue = self.getCurrentValue(width: width) {
+                //                        self.chartValue.currentValue = currentValue
+                //                        self.chartValue.interactionInProgress = true
+                //                    }
+            })
+                        .onEnded({ value in
+                //                    self.chartValue.interactionInProgress = false
+                self.touchLocation = -1
+            })
+            )
         }
     }
     
@@ -81,13 +84,13 @@ public struct BarChart: View {
     ///   - touchLocation: fraction of width where touch is happening
     ///   - index: index into data array
     /// - Returns: a scale larger than 1.0 if in bounds; 1.0 (unscaled) if not in bounds
-    //    func getScaleSize(touchLocation: CGFloat, index: Int) -> CGSize {
-    //        if touchLocation > CGFloat(index)/CGFloat(chartData.data.count) &&
-    //            touchLocation < CGFloat(index+1)/CGFloat(chartData.data.count) {
-    //            return CGSize(width: 1.4, height: 1.1)
-    //        }
-    //        return CGSize(width: 1, height: 1)
-    //    }
+    func getScaleSize(touchLocation: CGFloat, index: Int) -> CGSize {
+        if touchLocation > CGFloat(index) / CGFloat(chartDataset.data.count) &&
+            touchLocation < CGFloat(index+1) / CGFloat(chartDataset.data.count) {
+            return CGSize(width: 1.4, height: 1.1)
+        }
+        return CGSize(width: 1, height: 1)
+    }
     //
     /// Get data value where touch happened
     /// - Parameter width: width of chart
@@ -98,35 +101,34 @@ public struct BarChart: View {
     //        return self.chartData.values[index]
     //    }
 }
-struct MyPreviewProvider_Previews: PreviewProvider {
-    @ObservedObject static var data: ChartDataset = .init(labels: [String](), data: [
-        .init(data: [1, 3.0, 5, 10],
-                                 label: "data 1",
-                                 backgroundColor: .init(.sRGB, red: 1, green: 0, blue: 0, opacity: 0.2),
-                                 borderColor: .init(.sRGB, red: 1, green: 0, blue: 0, opacity: 0.8)),
-        .init(data: [2, 7.0, 1, 5],
-                                 label: "data 2",
-                                 backgroundColor: .init(.sRGB, red: 0, green: 1, blue: 0, opacity: 0.2),
-                                 borderColor: .init(.sRGB, red: 0, green: 1, blue: 0, opacity: 0.8)),
-        .init(data: [4, 2.0, 2, 9],
-                                 label: "data 3",
-                                 backgroundColor: .init(.sRGB, red: 0, green: 0, blue: 1, opacity: 0.2),
-                                 borderColor: .init(.sRGB, red: 0, green: 0, blue: 1, opacity: 0.8))
-    ])
+struct Barchart_Previews: PreviewProvider {
+    @ObservedObject static var data: ChartDataset = .init(labels: [String](), data: [])
 
-    static var options: ChartOptions = .init(dataset: .init(showValue: true), axes: .automatic, coordinateLine: .automatic)
+    static var options: ChartOptions = .init(dataset: .init(showValue: false), axes: .automatic, coordinateLine: .automatic)
     static var previews: some View {
         VStack {
-            BarChart(chartDataset: data)
+            BarChart()
+                .environmentObject(data)
                 .environmentObject(options)
+                .onAppear {
+                    Task {
+                        let data = (await getAvgVideoTimeByDateAPI()).suffix(50)
+                        self.data.labels = data.map({$0._id})
+                        self.data.data = [ChartData(data: data.map({Double($0.count)}), label: "1",
+                                                    backgroundColor: .init(.sRGB, red: 1, green: 0, blue: 0, opacity: 0.2),
+                                                    borderColor: .init(.sRGB, red: 1, green: 0, blue: 0, opacity: 0.8))]
+                    }
+                }
             Button {
-                
                 for i in 0..<data.data.count {
                     var newData: [Double] = []
                     for _ in 0..<data.data[i].data.count {
                         newData.append(Double.random(in: 0...100))
                     }
-                    data.data[i].data = .init(newData)
+                    withAnimation {
+                        data.data[i].data = .init(newData)
+                    }
+                    
                 }
             } label: {
                 Text("随机数据")
@@ -180,6 +182,7 @@ struct MyPreviewProvider_Previews: PreviewProvider {
                 }
             }
         }
+        
     }
 }
 
