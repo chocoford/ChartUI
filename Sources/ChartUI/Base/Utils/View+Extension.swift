2098892495d57b51a98fba26796b86d61cd57482
chocoford <504001583@qq.com>
2022-01-19 13:34:50 +0800

    add @ViewBuilder func `if`

--- Sources/ChartUI/Base/Utils/View+Extension.swift
+++ Sources/ChartUI/Base/Utils/View+Extension.swift
@@ -8,4 +8,18 @@ extension View {
     public func chartStyle(_ style: ChartStyle) -> some View {
         self.environmentObject(style)
     }
+    
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: @autoclosure () -> Bool, transform: (Self) -> Content) -> some View {
+        if condition() {
+            transform(self)
+        } else {
+            self
+        }
+    }
 }
+
