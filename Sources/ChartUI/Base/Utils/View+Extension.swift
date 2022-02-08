3e3e78385ba30c649af441ff521deea7cf0eb259
chocoford <504001583@qq.com>
2022-02-08 11:40:17 +0800

    add if-else conditional view caluse

--- Sources/ChartUI/Base/Utils/View+Extension.swift
+++ Sources/ChartUI/Base/Utils/View+Extension.swift
@@ -6,7 +6,21 @@ extension View {
     ///   - condition: The condition to evaluate.
     ///   - transform: The transform to apply to the source `View`.
     /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-    @ViewBuilder func `if`<Content: View>(_ condition: @autoclosure () -> Bool, transform: (Self) -> Content) -> some View {
+    @ViewBuilder func `if`<TrueContent: View, FalseContent: View>(_ condition: @autoclosure () -> Bool,
+                                          transform: @escaping (Self) -> TrueContent,
+                                          falseTransform: ((Self) -> FalseContent)? = nil) -> some View {
+        if condition() {
+            transform(self)
+        } else if falseTransform != nil {
+            falseTransform!(self)
+        } else {
+            self
+        }
+    }
+    
+    
+    @ViewBuilder func `if`<TrueContent: View>(_ condition: @autoclosure () -> Bool,
+                                          transform: @escaping (Self) -> TrueContent) -> some View {
         if condition() {
             transform(self)
         } else {
@@ -29,4 +43,41 @@ extension View {
         }
     }
 }
+extension _ConditionalContent {
 
+}
+
+//struct ConditionView<TrueContent: View, FalseContent: View>: View {
+//    var trueContent: TrueContent
+//    var falseContent: FalseContent
+//
+//    init(_ view: _ConditionalContent<TrueContent, FalseContent>){
+//        self.content = view
+//    }
+//
+//    var body: some View {
+//        self.content
+//    }
+//
+//    /// Applies the given transform if the given condition evaluates to `true`.
+//    /// - Parameters:
+//    ///   - condition: The condition to evaluate.
+//    ///   - transform: The transform to apply to the source `View`.
+//    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+//    @ViewBuilder func `elseif`(_ condition: @autoclosure () -> Bool, transform: (Self) -> Self) -> Self {
+//        if condition() {
+//            transform(self)
+//        } else {
+//            self
+//        }
+//    }
+//
+//    /// Applies the given transform if the given condition evaluates to `true`.
+//    /// - Parameters:
+//    ///   - condition: The condition to evaluate.
+//    ///   - transform: The transform to apply to the source `View`.
+//    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+//    @ViewBuilder func `else`<Content: View>(transform: (Self) -> Content) -> Self {
+//        transform(self)
+//    }
+//}
